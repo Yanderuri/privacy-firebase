ad3dcb6879d74d230e164c622937f44d4fe1042a
NekoNaozumi <huanmai101@gmail.com>
2021-11-23 11:58:13 -0500

    Ello

---
+++ app/src/main/java/com/example/privacy_firebase/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.privacy_firebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mAuth = FirebaseAuth.getInstance();
+        setContentView(R.layout.activity_main);
+    }
+    @Override
+    public void onStart(){
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+
+        }
+    }
+}
