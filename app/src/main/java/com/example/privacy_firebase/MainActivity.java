50e337aff1be446a39d623617f299ffc308dc38f
bruh <huanmai101@gmail.com>
2021-12-20 12:41:55 -0500

    Checking input instead of checking field

--- app/src/main/java/com/example/privacy_firebase/MainActivity.java
+++ app/src/main/java/com/example/privacy_firebase/MainActivity.java
@@ -96,8 +96,8 @@ public class MainActivity extends AppCompatActivity {
             throw new Exception(String.format("%s has to be at least 8 characters",field));
         }
         else if (field.equalsIgnoreCase("email")){
-            if (!input.matches("\"^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$\"")){
-                throw new Exception("Invalid email");
+            if (!input.matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")){
+                throw new Exception("Invalid email formatting");
             }
         }
         return input;
